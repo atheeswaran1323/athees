127a9c1ff30ecb9bf61361ebfcd1d708a571dc05
atheeswaran1323 <anishsoundararajan@gmail.com>
2022-08-19 17:14:46 +0530

    next project

--- FindSquareValue.java
+++ FindSquareValue.java
@@ -2,7 +2,7 @@ package forloop;
 
 public class FindSquareValue {
 	public static void main (String[]args) {
-		for(int i=2;i<=10;i++){
+		for(int i=2;i<=20;i++){
 			System.out.println("Square of "+i+" is "+(i*i));
 		}
 	}
